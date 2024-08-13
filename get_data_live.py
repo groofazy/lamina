15cdc77925ae0e67df4dce882777e0a7caafde03
Ryan Gamboa <gabe.gamboa05@gmail.com>
2024-08-12 21:59:52 -0700

    adding a bunch of data.html files

--- get_data_live.py
+++ get_data_live.py
@@ -27,8 +27,53 @@ def get_html(url, selector, sleep=5, retries=5):
             break
     return html # return none if fail, return html if success
 
-season = 2016
+def scrape_season(season):
+    url = f"https://www.basketball-reference.com/leagues/NBA_{season}_games.html"
+    html = get_html(url, "#content .filter")
 
-url = f"https://www.basketball-reference.com/leagues/NBA_{season}_games.html"
+    soup = BeautifulSoup(html, features="html.parser")
+    links = soup.find_all("a") #finds anchor tags
+    href = [l["href"] for l in links]
+    standings_pages = [f"https://basketball-reference.com{l}" for l in href]
 
-html = get_html(url, "#content .filter")
+    for url in standings_pages:
+        save_path = os.path.join(STANDINGS_DIR, url.split("/")[-1])
+        if os.path.exists(save_path):
+            continue
+
+        html = get_html(url, "#all_schedule")
+        with open(save_path, "w+", encoding="utf-8") as f:
+            f.write(html)
+
+#for season in SEASONS: # loop to scrape through each season
+    #scrape_season(season)
+
+standings_files = os.listdir(STANDINGS_DIR)
+
+def scrape_game(standings_file):
+    with open(standings_file, 'r') as f:
+        html = f.read()
+
+    soup = BeautifulSoup(html, features="html.parser")
+    links = soup.find_all("a")
+    hrefs = [l.get("href") for l in links]
+    box_scores = [l for l in hrefs if l and "boxscore" in l and ".html" in l]
+    box_scores = [f"https://www.basketball-reference.com{l}" for l in box_scores]
+
+    for url in box_scores:
+        save_path = os.path.join(SCORES_DIR, url.split("/") [-1])
+        if os.path.exists(save_path):
+            continue
+
+        html = get_html(url, "#content")
+        if not html:
+            continue
+        with open(save_path, "w+", encoding="utf-8") as f:
+            f.write(html)
+
+standings_files = [s for s in standings_files if ".html" in s] # filter dstore
+
+for f in standings_files:
+    filepath = os.path.join(STANDINGS_DIR, f)
+    
+    scrape_game(filepath)
---
+++ parse_data.py
@@ -0,0 +1,93 @@
+import os
+import pandas as pd
+from bs4 import BeautifulSoup
+
+SCORE_DIR = "data/scores"
+
+box_scores = os.listdir(SCORE_DIR)
+
+box_scores = [os.path.join(SCORE_DIR, f) for f in box_scores if f.endswith(".html")]
+
+def parse_html(box_score): # parses and cleans up html
+    with open(box_score) as f:
+        html = f.read()
+
+    soup = BeautifulSoup(html)
+    [s.decompose() for s in soup.select("tr.over_header")]
+    [s.decompose() for s in soup.select("tr.thead")]
+    return soup
+
+def read_line_score(soup):
+    line_score = pd.read_html(str(soup), attrs={"id": "line_score"}) # reads for html attr tag "id = line_score" and sends it back as pd dataframe
+    cols = list(line_score.colums)
+    cols[0] = "team"
+    cols[-1] = "total"
+    line_score.columns = cols
+
+    line_score = line_score[["team", "total"]]
+    return line_score
+
+def read_stats(soup, team, stat):
+    df = pd.read_html(str(soup), attrs={"id": f"box-{team}-game-{stat}"}, index_col=0) [0]
+    df = df.apply(pd.to_numeric, errors="coerce")
+    return df
+
+def read_season_info(soup):
+    nav = soup.select("#bottom_nav_container")[0]
+    hrefs = [a["href"] for a in nav.find_all("a")] # finding all anchor tags
+    season = os.path.basename(hrefs[1]).split("_")[0]
+    return season
+
+# main parse loop
+
+base_cols = None
+games = []
+for box_scores in box_scores:
+    box_score = box_scores[0]
+    soup =parse_html(box_score)
+    line_score = read_line_score(soup)
+    teams = list(line_score["team"])
+
+    summaries = []
+    for team in teams: # creating summary for single team
+        basic = read_stats(soup, team, "basic")
+        advanced = read_stats(soup, team, "advanced")
+
+        totals = pd.concat([basic.iloc[-1,:], advanced.iloc[-1,:]])
+        totals.index = totals.index.str.lower()
+
+        maxes = pd.concat([basic.iloc[:-1,:].max(), advanced.iloc[:-1,:].max()])
+        maxes.index = maxes.index.str.lower() + "_max"
+
+        summary = pd.concat([totals, maxes])
+
+        if base_cols is None:
+            base_cols = list(summary.index.drop_duplicates(keep="first"))
+            base_cols = [b for b in base_cols if "bpm" not in b]
+        
+        summary = summary[base_cols]
+
+        summaries.append(summary)
+    summary = pd.concat(summaries, axis=1).T
+
+    game = pd.concat([summary, line_score], axis=1) # adds team and team total points to df
+
+    game["home"] = [0,1] # 0 is home, 1 is away
+    game_opp = game.iloc[::-1].reset_index()# first row is now second row 
+    game_opp.columns += "opp"
+
+    full_game = pd.concat([game, game_opp], axis=1)
+
+    full_game["season"] = read_season_info(soup) # displays season in df
+
+    full_game["date"] = os.path.basename(box_score) [:8]
+    full_game["date"] = pd.to_datetime(full_game["date"], format="%Y%m%d")
+
+    full_game["won"] = full_game["total"] > full_game["total_opp"]
+    games.append(full_game)
+
+    if len(games) % 100 == 0:
+        print(f"{len(games)} / len{box_scores}")
+
+#concat all games together
+games_df = pd.concat(games, ignore_index=True)
